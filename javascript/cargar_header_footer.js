2317aa2261f5be1aeb0c312a8cf477440704eee0
maccorin <38734978+maccorin@users.noreply.github.com>
2025-05-10 01:41:36 -0300

    commit agregue redes

--- javascript/cargar_header_footer.js
+++ javascript/cargar_header_footer.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded",function()
 {
-fetch('./html/header.html')
+fetch('../html/header.html')
 .then(response=>response.text())
 .then(data=>{
     document.getElementById('header').innerHTML=data;   
 });
 
-fetch('./html/footer.html')
+fetch('../html/footer.html')
 .then(response=>response.text())
 .then(data=>{
     document.getElementById('footer').innerHTML=data;   

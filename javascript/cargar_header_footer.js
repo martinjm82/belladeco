183740499c34975704eb955bcf0f32246a5e4eab
maccorin <38734978+maccorin@users.noreply.github.com>
2025-05-27 23:09:07 -0300

    commit agregue fix spinner

--- javascript/cargar_header_footer.js
+++ javascript/cargar_header_footer.js
@@ -1,4 +1,28 @@
-document.addEventListener("DOMContentLoaded",function()
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.getElementById("header");
+    const footer = document.getElementById("footer");
+  
+    Promise.all([
+      fetch("../html/header.html")
+        .then(res => res.text())
+        .then(data => {
+          header.innerHTML = data;
+        }),
+      fetch("../html/footer.html")
+        .then(res => res.text())
+        .then(data => {
+          footer.innerHTML = data;
+        })
+    ]).then(() => {
+      // Ocultar el loader cuando ambos hayan terminado
+      const loader = document.getElementById("loader");
+      if (loader) {
+        loader.style.display = "none";
+      }
+    });
+  });
+
+  /*document.addEventListener("DOMContentLoaded",function()
 {
 fetch('../html/header.html')
 .then(response=>response.text())
@@ -11,4 +35,4 @@ fetch('../html/footer.html')
 .then(data=>{
     document.getElementById('footer').innerHTML=data;   
 });
-});
+});*/
